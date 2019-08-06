fc7c52ec1dfa899016fddd5c85e98957af9a66e9
TaeSun94 <tyzlddy@naver.com>
2019-08-06 16:22:17 +0900

    update result

--- result.js
+++ result.js
@@ -12,21 +12,22 @@ result.successFalse = function(err, message){
   return{
     result:false,
     message:message,
-  //  errors:(err)? result.parseError(err):null,
+    errors:(err)? result.parseError(err):null,
   };
 };
-/*
+
 result.parseError = function(errors){
   var parsed = {};
   if(errors.name == 'ValidationError'){
     for(var name in errors.errors){
-      var errorname = error.errors[name];
+      var errorname = errors.errors[name];
       parsed[name] = { message: errorname.message};
     }
+    return parsed;
   }
   else{
     parsed.unhandled = errors;
   }
 };
-*/
+
 module.exports = result;
