b08f8a833a423587a3343bcf2ec4e0c03725a9fc
TaeSun94 <tyzlddy@naver.com>
2019-08-12 17:21:01 +0900

    first commit

--- v1/app/klaytnsori/mypage/mypage.js
+++ v1/app/klaytnsori/mypage/mypage.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var mypage = express.Router();
 var result = require('./../../../../result');
-var Caver = require('caver-js');
-var caver = new Caver('https://api.baobab.klaytn.net:8651/');
-var db = require('./../../../../database.js');
 
 /*
 *transaction API
@@ -30,16 +27,8 @@ mypage.get('/transaction', function(req,res,next){
   if(!isValid) return res.json(result.successFalse(validationError));
   else next();
 }, function(req,res,next){
-  var u_wallet_address;
-  var b_list = new Array();
-  //DB에서 해당 session_id로 들어온 사용자의 email을 확인 후 caver로 거래 내역 조회
+  var _session = req.query.session_id;
 
-  var data = new Array();
-  for(var i = 0 ; i < b_list.length ; i++)
-  {
-    data[i] = caver.klay.getBlock(b_list[i]);
-  }
-  return res.json(result.successTrue(data));
 });
 
 /*
@@ -66,24 +55,8 @@ mypage.get('/my_question_list', function(req,res,next){
   if(!isValid) return res.json(result.successFalse(validationError));
   else next();
 }, function(req,res,next){
-  db.klaytndb.connect();
   var session_id = req.query.session_id;
-  var params = [session_id];
-  var sql = "SELECT email FROM userSession WHERE session_id = ?";
-  db.klaytndb.query(sql, params, function(err, rows, fields){
-    if(err) return res.json(result.successFalse(err));
-    else{
-      var params2 = [result[0].email];
-      var sql2 = "SELECT question.question_title, question.question_content, question.klay, category.category FROM question JOIN category WHERE question.email = ? AND question.category_num = category.category_num";
-      db.klaytndb.query(sql2, params2, function(err, rows, fields){
-        if (err) return res.json(result.successFalse(err));
-        else{
-          db.klaytndb.end();
-          return res.json(result.successTrue(rows));
-        }
-      });
-    }
-  });
+
 });
 
 /*
@@ -110,24 +83,9 @@ mypage.get('/my_answer_list', function(req,res,next){
   if(!isValid) return res.json(result.successFalse(validationError));
   else next();
 }, function(req,res,next){
-  db.klaytndb.connect();
   var session_id = req.query.session_id;
-  var params = [session_id];
-  var sql = "SELECT email FROM userSession WHERE session_id = ?";
-  klaytndb.query(sql, params, function(err, rows, fields){
-  if (err) return res.json(result.successFalse(err));
-  else{
-    var params2 = [result[0].email];
-    var sql2 = "SELECT question.question_title ,answer.answer_content, answer.is_selected FROM question JOIN answer ON answer.email = ?";
-    db.klaytndb.query(sql2, params2, function(err, rows, fields){
-      if(err) return res.json(result.successFalse(err));
-      else{
-        db.klaytndb.end();
-        return res.json(result.successTrue(rows));
-      }
-    });
-   }
-  });
+
+
 });
 
 /*
@@ -154,24 +112,8 @@ mypage.get('/my_like_list', function(req,res,next){
   if(!isValid) return res.json(result.successFalse(validationError));
   else next();
 }, function(req,res,next){
-  db.klaytndb.connect();
   var session_id = req.query.session_id;
-  var params = [session_id];
-  var sql = "SELECT email FROM userSession WHERE session_id = ?";
-  db.klaytndb.query(sql, params, function(err, rows, fields){
-    if(err) return res.json(result.successFalse(err));
-    else{
-      var params2 = [result[0].email];
-      var sql2 = "SELECT question_num, answer_num FROM userLike WHERE email = ?";
-      db.klaytndb.query(sql2, params2, function(err, rows, fields){
-        if(err) return res.json(result.successFalse(err));
-        else{
-          db.klaytndb.end();
-          return res.json(result.successTrue(rows));
-        }
-      });
-     }
-  });
+
 });
 
 /*
@@ -196,29 +138,8 @@ mypage.get('/my_remain_klay', function(req,res,next){
   if(!isValid) return res.json(result.successFalse(validationError));
   else next();
 }, function(req,res,next){
-  db.klaytndb.connect();
   var session_id = req.query.session_id;
-  var params = [session_id];
-  var sql = "SELECT email FROM userSession WHERE session_id = ?";
-  db.klaytndb.query(sql, params, function(err, rows, fields){
-    if(err) return res.json(result.successFalse(err));
-    else{
-      var params2 = [result[0].email];
-      var sql2 = "SELECT wallet_address FROM userInfo WHERE email = ?";
-      db.klaytndb.query(sql2, params2, function(err, rows, fields){
-        if(err) return res.json(result.successFalse(err));
-        else{
-          var u_account_address = rows[0].wallet_address;
-          var u_klay = caver.klay.getBalance('u_account_address');
-          var data = {
-            klay : u_klay
-          };
-          db.klaytndb.end();
-          return res.json(result.successTrue(data));
-        }
-      });
-     }
-  });
+
 });
 
 module.exports = mypage;
