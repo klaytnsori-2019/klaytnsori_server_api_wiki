9a03d4f6d87b415f5085df4528b234724d401dde
zustkeeper <zustkeeper@naver.com>
2019-08-13 22:18:52 +0900

    abi-decoder

--- v1/app/caver/membershipCaver.js
+++ v1/app/caver/membershipCaver.js
@@ -4,15 +4,21 @@ var membershipCaver = {};
 
 membershipCaver.addAccount = function(_privateKey){
 
+    cav.caver.klay.accounts.wallet.add(_privateKey);
+
     return true;
 }
 
 membershipCaver.removeAccount = function(_address){
 
+    cav.caver.klay.accounts.wallet.remove(_address);
+
     return true;
 }
 
 membershipCaver.createAccount = function(){
+
+    var account = cav.caver.klay.accounts.create();
 
     return account;
 }
