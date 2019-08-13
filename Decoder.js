7485a0ae8ab3fefd018c00637543feb5db28cdaf
zustkeeper <zustkeeper@naver.com>
2019-08-13 23:55:44 +0900

    modified

--- Decoder.js
+++ Decoder.js
@@ -5,7 +5,7 @@ const ABI = config.ABI;
 abiDecoder.addABI(ABI);
 
 var decoder = function(data){
-    return abiDecoder.decodeMethod(data);
-}
+    return abiDecoder.decodeMethod(data).params[0].value;
+};
 
 module.exports = decoder;
