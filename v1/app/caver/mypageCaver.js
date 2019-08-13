b2490f484a2053ac68979109356602d6e2cc94b4
zustkeeper <zustkeeper@naver.com>
2019-08-13 23:56:12 +0900

    tx, bal commit

--- v1/app/caver/mypageCaver.js
+++ v1/app/caver/mypageCaver.js
@@ -1,17 +1,43 @@
-var cav = require('../../../Klaytn')
+var cav = require('../../../Klaytn');
+var decoder = require('../../../Decoder');
 
 var mypageCaver = {};
 
 mypageCaver.showTransactions = function(_blockNumList){
 
-    
+    var transactions = new Array;
+
+    for(var txHash in _blockNumList){
+        var receipt = cav.caver.klay.getTransactionReceipt(txHash);
+        var b_isReceived = false;
+        var value = receipt.value;
+
+        if(recepit.value == 0){
+            b_isReceived = true;
+            value = decoder(receipt.input);
+        }
+
+        cav.caver.klay.getBlock().then((result)=>{
+            var data = {
+                "isReceived" : b_isReceived,
+                "from" : receipt.from,
+                "to" : receipt.to,
+                "value" : value,
+                "timestamp" : result.timestamp
+            }
+            transactions.push(data);
+        });
+    }
 
     return transactions;
 }
 
 mypageCaver.showMyKlay = function(_address){
 
-    return balance;
+    cav.caver.klay.getBalance(_address).then((result)=>{
+        var balance = cav.caver.utils.fromPeb(result, 'KLAY');
+        return balance;
+    });
 }
 
 module.exports = mypageCaver;
