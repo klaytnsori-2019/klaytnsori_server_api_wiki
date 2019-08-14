882cd80cb67a04154d69436bc0fa2439d7a6457b
TaeSun94 <tyzlddy@naver.com>
2019-08-14 13:52:51 +0900

    update

--- v1/app/klaytnsori/membership/membership.js
+++ v1/app/klaytnsori/membership/membership.js
@@ -2,7 +2,8 @@ var express = require('express');
 var membership = express.Router();
 var result = require('./../../../../result.js');
 var mail = require('./../../../../mail.js');
-
+var caver = require('../../caver/membershipCaver.js');
+var db = require('./../../../../klaytndb.js');
 /*
 *Log-in API
 *Request
@@ -29,16 +30,24 @@ membership.post('/login', function(req, res, next){
     else next();
 },
  function(req, res){
-   var u_email = req.body.email;
-   var u_pw = req.body.password;
+   var userEmail = req.body.email;
+   var userPassword = req.body.password;
    //email과 pw를 보내 디비에서 privatekey를 가져옴
-
+   var userPrivatekey = .login(userEmail, userPassword);
    //privatekey로 caver에서 wallet 추가
-
+   var caverOk = caver.addAccount(userPrivatekey);
    //db에 email을 보내서 세션 받기.
+   if(caverOk){
+     var userSession = db.login2(u_email, u_ pw);
+     var data = {
+       "session_id" : userSession
+     };
+     return res.json(result.successTrue(data));
+   }
+   else {
+     //wallet추가 err처리
 
-   var data = { message : 'Wellcome to klaytnsori service!'};
-   return res.json(result.successTrue(data));
+   }
  });
 
 /*
@@ -49,15 +58,21 @@ membership.post('/login', function(req, res, next){
 *if success log-out, return in result.json result = true.
 */
 membership.post('/logout', function (req, res) {
-    var logout_session = req.body.session_id;
+    var userSession = req.body.session_id;
     //DB에서 session_id로 들어온거로 이메일을 확인 후 해당 계좌를 반환
-
+    var userAccount = db.logout1(userSession);
     //caver에서 들어온 계좌를 wallet에서 제거
-
+    var caverOk = caver.removeAccount(userAccount);
     //DB에서 session_id 행 삭제
-
-    var data = { message: 'Thanks to use our service' };
-    return res.json(result.successTrue(data));
+    if(caverOk){
+      db.logout2(userSession);
+      var data = { message: 'Thanks to use our service' };
+      return res.json(result.successTrue(data));
+    }
+    else{
+      var data = { message : 'Can not Remove account from wallet.'};
+      return res.json(result.successFalse(data));
+    }
 });
 
 /*
@@ -94,18 +109,27 @@ membership.post('/signup', function(req, res, next){
     if (!isValid) return res.json(result.successFalse(validationError));
     else next();
 }, function(req, res, next){
-    var u_email = req.body.email;
-    var u_pw = req.body.password;
-    var u_nick = req.body.nickname;
+    var userEmail = req.body.email;
+    var userPassword = req.body.password;
+    var userNickname = req.body.nickname;
     //DB에서 해당 이메일 중복 여부 확인 후 count값으로 리턴
-
-
-    var data = {
-      email : u_email,
-      password : u_pw,
-      nickname : u_nick
-    };
-    return res.json(result.successTrue(data));
+    var overlapCount = db.signup1(userEmail);
+    if(overlap_count == 0){
+      var data = {
+        "email" : userEmail,
+        "password" : userPassword,
+        "nickname" : userNickname
+      };
+      return res.json(result.successTrue(data));
+    }
+    else {
+      var emailError = {
+          "name": 'email 중복',
+          "errors": {}
+      };
+      emailError.errors.email = { message: 'Another user is using same email' };
+      return res.json(result.successFalse(emailError));
+    }
 });
 
 /*
@@ -131,25 +155,33 @@ membership.post('/find_pw_auth_code', function(req, res, next){
   if (!isValid) return res.json(result.successFalse(validationError));
   else next();
 },function(req,res){
-    var u_email = req.body.email;
+    var userEmail = req.body.email;
     //DB에서 해당 email이 있는지 확인 후 count로 리턴
-
-
-    var authorize_text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for(var i = 0; i < 6 ; i++){
-      authorize_text += possible.charAt(Math.floor(Math.random() * possible.length));
+    var emailCount = db.find_pw_auth_identity1(userEmail);
+    if(emailCount == 0){
+      var authorizeText = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      for(var i = 0; i < 6 ; i++){
+        authorizeText += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      //DB에서 해당 이메일과 인증코드를 인증테이블에 저장후 true리턴
+      var dbOk = db.find_pw_auth_identity2(userEmail, authorizeText);
+      if(dbOk){
+        var data = {
+          email : userEmail,
+          authorize_text : authorizeText
+        };
+        mail.transporter.sendMail(mail.mailOption(userEmail,authorizeText), function(err, info){
+            return res.json(result.successTrue(data));
+          });
+      }
+      else{
+        //db저장 오류
+      }
     }
-
-    //DB에서 해당 이메일과 인증코드를 인증테이블에 저장후 true리턴
-
-    var data = {
-      email : u_email,
-      authorize_text : authorize_text
-    };
-    mail.transporter.sendMail(mail.mailOption(u_email,authorize_text), function(err, info){
-        return res.json(result.successTrue(data));
-    });
+    else{
+      //email존재X
+    }
 });
 
 /*
@@ -179,10 +211,10 @@ membership.post('/find_pw_auth_identity', function (req, res, next) {
     if(!isValid) return res.json(result.successFalse(validationError));
     else next();
 },function(req, res, next){
-  var u_email = req.body.email;
+  var userEmail = req.body.email;
   var string1 = req.body.authorize_text;
   //DB에서 해당 이메일로 저장되어 있는 인증코드 리턴
-
+  var string2 = db.auth_identity_code(userEmail);
   if(string1 != string2){
     var codeError = {
         "name": 'Authorize code Error',
@@ -191,12 +223,11 @@ membership.post('/find_pw_auth_identity', function (req, res, next) {
     codeError.errors = { message: 'Diffrent authorize text' };
     return res.json(result.successFalse(codeError));
   }
-
   else{
     //DB에서 해당 이메일의 매칭되어있는 password 리턴
-
+    var userPassword = db.find_pw_auth_identity4 (userEmail);
     var data = {
-      password : _passwd
+      "password" : userPassword
     };
     return res.json(result.successTrue(data));
   }
@@ -228,12 +259,17 @@ membership.post('/modify_pw', function (req, res, next) {
     if (!isValid) return res.json(result.successFalse(validationError));
     else next();
 }, function (req, res) {
-    var _session = req.body.session_id;
-    var m_pw = req.body.password;
+    var userSession = req.body.session_id;
+    var newPassword = req.body.password;
     //DB에서 해당 세션으로 이메일을 확인한 뒤 새로들어온 password로 변경
-
-    var data = { message : 'Success to modify your password!'};
-    return res.json(result.successTrue(data));
+    var dbOk = db.modify_pw(userSession, newPassword);
+    if(dbOk){
+      var data = { message : 'Success to modify your password!'};
+      return res.json(result.successTrue(data));
+    }
+    else{
+      //db변경 오류
+    }
 });
 
 /*
@@ -263,25 +299,28 @@ membership.post('/authorize_code', function (req, res, next) {
     if (!isValid) return res.json(result.successFalse(validationError));
     else next();
 }, function (req, res, next) {
-    var u_email = req.body.email;
-    var u_pw = req.body.password;
-    var u_nick = req.body.nickname;
-    var authorize_text = "";
+    var userEmail = req.body.email;
+    var userPassword = req.body.password;
+    var userNickname = req.body.nickname;
+    var authorizeText = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (var i = 0; i < 6 ; i++) {
-        authorize_text += possible.charAt(Math.floor(Math.random() * possible.length));
+        authorizeText += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     //DB에서 해당 이메일과 인증번호를 인증테이블에 저장 후 true 리턴
-    var data = {
-        "email" : u_email,
-        "password" : u_pw,
-        "nickname" : u_nick,
-        "authorize_text": authorize_text
-    };
+    var dbOk = db.find_pw_auth_identity2(userEmail, authorizeText);
+    if(dbOk){
+      var data = {
+          "email" : userEmail,
+          "password" : userPassword,
+          "nickname" : userNickname,
+          "authorize_text": authorizeText
+        };
 
-    mail.transporter.sendMail(mail.mailOption(u_email,authorize_text), function(err, info){
-        return res.json(result.successTrue(data));
-    });
+        mail.transporter.sendMail(mail.mailOption(u_email,authorize_text), function(err, info){
+          return res.json(result.successTrue(data));
+        });
+    }
 });
 
 /*
@@ -316,19 +355,24 @@ membership.post('/authorize_identity', function (req, res, next) {
     if(!isValid) return res.json(result.successFalse(validationError));
     else next();
 },function(req, res, next){
-  var u_email = req.body.email;
-  var u_pw = req.body.password;
-  var u_nick = req.body.nickname;
+  var userEmail = req.body.email;
+  var userPassword = req.body.password;
+  var userNickname = req.body.nickname;
   var string1 = req.body.authorize_text;
   //DB에서 해당 이메일로 들어온 인증코드 리턴
-
+  var string2= db.auth_identity_code(userEmail);
   if(string1 == string2){
     //caver에서 새로운 계좌 생성 후 address와 privatekey 리턴
-
+    var userCaver = caver.createAccount();
     //DB에 해당 사용자의 정보를 모두 저장.
-
-    var data = {};
-    return res.json(result.successTrue(data));
+    var dbOk = db.signup2(userEmail, userPassword, userNickname, userCaver.address, userCaver.privateKey);
+    if(dbOk){
+      var data = {};
+      return res.json(result.successTrue(data));
+    }
+    else {
+      //db 오류
+    }
   }
   else{
     var codeError = {
@@ -338,8 +382,6 @@ membership.post('/authorize_identity', function (req, res, next) {
     codeError.errors = { message: 'Diffrent authorize text' };
     return res.json(result.successFalse(codeError));
   }
-    }
-  });
 });
 
 module.exports = membership;
