bc8a11c987d16e2549f53aaf68a5235bb7f46a84
zustkeeper <zustkeeper@naver.com>
2019-08-18 01:35:24 +0900

    questionCaver commit

--- v1/app/klaytnsori/caver/QuestionCaver.js
+++ v1/app/klaytnsori/caver/QuestionCaver.js
@@ -1,15 +1,61 @@
 var cav = require('../../../../Klaytn');
+var config = require('../../../../KlaytnConfig');
 
 var questionCaver = {};
 
-questionCaver.putReward = function(_address){
+questionCaver.putReward = async function(_address, _privateKey, _value){
 
-    return transaction;
+    var funcData = await cav.caver.klay.abi.encodeFunctionCall({
+        name: 'deposit',
+        type: 'function',
+        inputs: [],
+    }, []);
+
+    var rawTransaction = await cav.caver.klay.accounts.signTransaction({
+        type: 'FEE_DELEGATED_SMART_CONTRACT_EXECUTION',
+        from: _address,
+        to:   config.contractAddress,
+        data: funcData,
+        gas:  '300000',
+        value: caver.utils.toPeb(_value, 'KLAY'),
+    }, _privateKey)
+
+    cav.caver.klay.sendTransaction({
+        senderRawTransaction: rawTransaction,
+        feePayer: config.feePayerAddress,
+    }).then(function(receipt){
+        return receipt;
+    })
 }
 
-questionCaver.getReward = function(_address, _questionerAddress,_value){
+questionCaver.getReward = async function(_address, _privateKey, _questionerAddress,_value){
 
-    return true;
+    var funcData = await cav.caver.klay.abi.encodeFunctionCall({
+        name: 'transfer',
+        type: 'function',
+        inputs: [{
+            type: 'address',
+            name: '_questionerAddress'
+        },{
+            type: 'uint256',
+            name: '_value'
+        }]
+    }, [_questionerAddress, _value]);
+
+    var rawTransaction = await cav.caver.klay.accounts.signTransaction({
+        type: 'FEE_DELEGATED_SMART_CONTRACT_EXECUTION',
+        from: _address,
+        to:   config.contractAddress,
+        data: funcData,
+        gas:  '300000',
+    }, _privateKey);
+
+    cav.caver.klay.sendTransaction({
+        senderRawTransaction: rawTransaction,
+        feePayer: config.feePayerAddress,
+    }).then(function(receipt){
+        return receipt;
+    })
 }
 
 module.exports = questionCaver;
