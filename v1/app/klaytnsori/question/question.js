d80ce4b12a5eeea2679e0dce2ed7ff277057a192
TaeSun94 <tyzlddy@naver.com>
2019-08-27 15:17:08 +0900

    update question.js 통합완료 파일

--- v1/app/klaytnsori/question/question.js
+++ v1/app/klaytnsori/question/question.js
@@ -14,11 +14,15 @@ var db = require('./../../../../klaytndb.js');
 question.get('/category', function(req,res,next){
   //DB에서 카테고리 정보 리스트 형태로 반환.
   db.category((rows)=>{
-    var data = new Array;
-    for(var i in rows){
-      data.push(rows[i]);
+    if(rows == false){
+      var dbError = {
+          "name": 'DB',
+          "errors": {}
+      };
+      dbError.errors.db = { message: 'Cannot find category in category table' };
+      return res.json(result.successFalse(dbError));
     }
-    return res.json(result.successTrue(data));
+    return res.json(result.successTrue(rows));
   });
 });
 
@@ -69,28 +73,52 @@ question.post('/insert_question', function(req,res,next){
   var questionTitle = req.body.question_title;
   var questionContent = req.body.question_content;
   var questionCategory = req.body.category;
+  //questionKlay = String(questionKlay);
   //DB에서 세션 아이디로 해당 유저의 계좌를 가져옴
   //caver에서 해당 유저의 계좌와 klay로 전송 후 트랜잭션 명세서 리턴
   //DB에 필요 정보 저장 후 question_id 리턴
   db.getWalletaddressAndPK(userSession, (rows)=>{
-    var userAccount = rows[0].wallet_address;
-    var userPrivatekey = rows[0].private_key;
-    caver.putReward(userAccount, userPrivatekey, questionKlay).then((receipt)=>{
-      var transactionHexTime = receipt.timestamp;
-      var transactionUnixTime = parseInt(transactionHexTime, 16);
-      var transactionDate = new Date(transactionUnixTime);
-      var transactionHash = receipt.transactionHash;
-      var questionRemainKlay = receipt.klay;
-      db.insert_question2(userAccount, transactionHash);
-      db.insert_question1(userSession, questionTitle, questionRemainKlay, questionContent, questionCategory, transactionDate, (rows)=>{
-        var questionId = rows[0].question_id;
-        var data = {
-          question_id : questionId
-        };
-        return res.json(result.successTrue(data));
+    if(rows == false){
+      var dbError = {
+          "name": 'DB',
+          "errors": {}
+      };
+      dbError.errors.db = { message: 'Cannot find session in session table' };
+      return res.json(result.successFalse(dbError));
+    }
+    else{
+      var userAccount = rows[0].wallet_address;
+      var userPrivatekey = rows[0].private_key;
+      caver.putReward(userAccount, userPrivatekey, questionKlay).then((receipt)=>{
+        var transactionHexTime = receipt.timestamp;
+        var transactionUnixTime = parseInt(transactionHexTime, 16);
+        var transactionDate = new Date(transactionUnixTime*1000+9*60*60*1000);
+        var transactionHash = receipt.transactionHash;
+        var hexKlay = receipt.value;
+        var questionRemainKlay = parseInt(hexKlay,16)/1000000000000000000;
+        db.insert_question2(userSession, transactionHash, (rows)=>{
+          var dbOk1 = rows;
+          if(dbOk1){
+            db.insert_question1(userSession, questionTitle, questionRemainKlay, questionContent, questionCategory, transactionDate, (rows)=>{
+              var questionId = rows[0].max;
+              var data = {
+                question_id : questionId
+              };
+              return res.json(result.successTrue(data));
+            });
+          }
+          else{
+            var dbError = {
+                "name": 'DB',
+                "errors": {}
+              };
+            dbError.errors.db = { message: 'Cannot insert information in question table' };
+            return res.json(result.successFalse(dbError));
+          }
+        });
       });
+     }
     });
-  });
 });
 
 /*
@@ -124,7 +152,17 @@ question.get('/show_question',function(req,res,next){
   var questionNum = req.query.question_id;
   //DB에서 질문 넘버로 질문 제목,클레이,내용,시간,질문자,상태,카테고리 리턴
   db.show_question(questionNum, (rows)=>{
-    return res.json(result.successTrue(rows));
+    if(rows == false){
+      var dbError = {
+          "name": 'DB',
+          "errors": {}
+      };
+      dbError.errors.db = { message: 'Cannot find question in question table' };
+      return res.json(result.successFalse(dbError));
+    }
+    else{
+      return res.json(result.successTrue(rows));
+    }
   });
 });
 
@@ -136,6 +174,7 @@ question.get('/show_question',function(req,res,next){
 *keyword : Not essentially request. division questions using keyword
 *question_state : Essentially request. division questions using question_state
 *select_enable : if question_state is false, essentially request. division questions using select_enable
+*default : if user select default List, show all question list.
 *Response
 *question_id : return question's id
 *category : return category
@@ -155,26 +194,48 @@ question.get('/question_list',function(req,res,next){
     isValid = false;
     validationError.errors.question_state = { message : '404 Not Found'};
   }
-  if(req.query.question_state == false)
-  {
-    if(!req.query.select_enable)
-    {
-      isValid = false;
-      validationError.errors.question_state = { message : '404 Not Found'};
-    }
+  if(!req.query.default){
+    isValid = false;
+    validationError.errors.default = { message : '404 Not Found'};
   }
   if(!isValid) return res.json(result.successFalse(validationError));
   else next();
 }, function(req,res,next){
-  var selectEnable = req.body.select_enable;
-  var questionState = req.body.question_state;
+  var questionState = req.query.question_state;
   var currentTime = new Date().getTime();
-  var dateLimit = 604800;
-  var questionCategory = req.body.category;
-  var questionSort = req.body.sort_num;
-  var questionKeyword = req.body.keyword;
-  db.function(questionState, currentTime, dateLimit, sort_num, questionKeyword, questionCategory, (rows)=>{
+  var questionDefault = req.query.default;
 
+  if(!req.query.category){
+    var questionCategory = 0;
+  }
+  else{
+    questionCategory = req.query.category;
+  }
+  if(!req.query.sort_num){
+    var questionSort = 0;
+  }
+  else{
+    questionSort = req.query.sort_num;
+  }
+  if(!req.query.keyword){
+    var questionKeyword = "%%";
+  }
+  else{
+    questionKeyword = req.query.keyword;
+  }
+
+  db.questionList(questionState, currentTime, questionDefault, questionSort, questionKeyword, questionCategory, (rows)=>{
+    if(rows == false){
+      var dbError = {
+          "name": 'DB',
+          "errors": {}
+      };
+      dbError.errors.db = { message: 'Cannot find question in question table' };
+      return res.json(result.successFalse(dbError));
+    }
+    else{
+      return res.json(result.successTrue(rows));
+    }
   })
 });
 
@@ -216,9 +277,20 @@ question.post('/insert_answer', function(req,res,next){
   var answerContent = req.body.answer_content;
   var questionNum = req.body.question_id;
   //DB에서 해당 질문 번호에 답변과 답변자 이메일 넣기.
-  db.insert_answer(userSession, answerContent, questionNum, (rows)=>{
-    var data = {message : 'Success to insert answer'};
-    return res.json(result.successTrue(data));
+  db.insertAnswer(userSession, answerContent, questionNum, (rows)=>{
+    var dbOk = rows;
+    if(dbOk){
+      var data = {message : 'Success to insert answer'};
+      return res.json(result.successTrue(data));
+    }
+    else{
+      var dbError = {
+          "name": 'DB',
+          "errors": {}
+      };
+      dbError.errors.db = { message: 'Cannot insert answer in answer table' };
+      return res.json(result.successFalse(dbError));
+    }
   });
 });
 
@@ -257,9 +329,20 @@ question.post('/insert_like', function(req,res,next){
   var questionNum = req.body.question_id;
   var answerNum = req.body.answer_id;
   //DB에 해당 질문 번호와 답변 번호와 유저 이메일을 테이블에 저장.
-  db.insert_like(userSession, questionNum, answerNum, (rows)=>{
-    var data = {message : 'Success to add like'};
-    return res.json(result.successTrue(data));
+  db.insertLike(userSession, questionNum, answerNum, (rows)=>{
+    var dbOk = rows;
+    if(dbOk){
+      var data = {message : 'Success to add like'};
+      return res.json(result.successTrue(data));
+    }
+    else{
+      var dbError = {
+          "name": 'DB',
+          "errors": {}
+      };
+      dbError.errors.db = { message: 'Cannot insert like in like table' };
+      return res.json(result.successFalse(dbError));
+    }
   });
 });
 
@@ -301,43 +384,112 @@ question.post('/select_answer',function(req,res,next){
   var questionNum = req.body.question_id;
   var answerNum = req.body.answer_id;
   var selectEnable = req.body.select_enable;
-  db.select_answer(questionNum, answerNum, (rows)=>{
-    var answerAccount = rows.account;
-    if(selectEnable){
-      db.function((rows)=>{
-        var answerPrivatekey;
-        var questionerAccount;
-        var questionKlay;
-        caver.getReward(answerAccount, answerPrivatekey, questionerAccount, questionKlay).then((receipt)=>{
-          var txHash = receipt.senderRawTransaction.transactionHash;
-          db.function(txHash, (rows)=>{
-            var data = { message : ''};
-            return res.json(result.successTrue(data));
-          });
+  db.selectAnswerOne(questionNum, answerNum, (rows)=>{
+    if(rows == false){
+      var dbError = {
+          "name": 'DB',
+          "errors": {}
+      };
+      dbError.errors.db = { message: 'Cannot find information in question table' };
+      return res.json(result.successFalse(dbError));
+    }
+    var answerAccount = rows.answer_wallet_address;
+    var answerPrivatekey = rows.answer_private_key;
+    var questionerAccount = rows. questioner_wallet_address;
+    var questionKlay = rows.klay;
+    if(selectEnable == 0){
+      questionKlay = String(questionKlay);
+      caver.getReward(answerAccount, answerPrivatekey, questionerAccount, questionKlay).then((receipt)=>{
+        var txHash = receipt.transactionHash;
+        db.registerTransaction(txHash, answerAccount, (rows)=>{
+          var dbOk = rows;
+          if(dbOk){
+            db.registerTransaction(txHash, questionerAccount,(rows)=>{
+              var dbOk1 = rows;
+              if(dbOk1){
+                return res.json(result.successTrue(dbOk1));
+              }
+              else{
+                var dbError = {
+                    "name": 'DB',
+                    "errors": {}
+                };
+                dbError.errors.db = { message: 'Cannot insert information in transaction table' };
+                return res.json(result.successFalse(dbError));
+              }
+            });
+          }
+          else{
+            var dbError = {
+                "name": 'DB',
+                "errors": {}
+            };
+            dbError.errors.db = { message: 'Cannot insert information in transaction table' };
+            return res.json(result.successFalse(dbError));
+          }
         });
       });
     }
-    else{
-      db.function((rows)=>{
-      var answerPrivatekey;
-      var questionerAccount;
-      var questionKlay;
+    else if(selectEnable == 1){
       var answerKlay = questionKlay*0.7;
+      answerKlay = String(answerKlay);
       caver.getReward(answerAccount, answerPrivatekey, questionerAccount, answerKlay).then((receipt)=>{
-        var txHash = receipt.senderRawTransaction.transactionHash;
-        db.function(txHash, (rows)=>{});
-      });
-      db.function((rows)=>{
-        var likeKlay = questionKlay*0.3/rows.length;
-        //like누른 사람들에게 보낼꺼
-        for(var i in rows){
-          caver.getReward(rows[i].account, rows[i].privateKey, questionerAccount, likeKlay).then((receipt)=>{
-            var txHash = receipt.senderRawTransaction.transactionHash;
-            db.function(txHash, (rows)=>{})
+        var txHash = receipt.transactionHash;
+        db.registerTransaction(txHash, answerAccount, (rows)=>{
+          var dbOk = rows;
+          if(dbOk){
+            db.registerTransaction(txHash, questionerAccount,(rows)=>{
+              var dbOk1 = rows;
+              if(!dbOk1){
+                var dbError = {
+                    "name": 'DB',
+                    "errors": {}
+                };
+                dbError.errors.db = { message: 'Cannot insert information in transaction table' };
+                return res.json(result.successFalse(dbError));
+              }
+            });
+          }
+          else{
+            var dbError = {
+                "name": 'DB',
+                "errors": {}
+            };
+            dbError.errors.db = { message: 'Cannot insert information in transaction table' };
+            return res.json(result.successFalse(dbError));
+          }
         });
-       }
       });
-     });
+      db.selectAnswerLike(answerNum, (rows)=>{
+        if(rows == false){
+          var dbError = {
+              "name": 'DB',
+              "errors": {}
+          };
+          dbError.errors.db = { message: 'Cannot find information in like table' };
+          return res.json(result.successFalse(dbError));
+        }
+        var likeKlay = questionKlay*0.3/rows.length;
+        likeKlay = String(likeKlay);
+        for(var i in rows){
+          caver.getReward(rows[i].wallet_address, rows[i].private_key, questionerAccount, likeKlay).then((receipt)=>{
+            var txHash = receipt.transactionHash;
+            db.registerTransaction(txHash, rows[i].wallet_address, (rows)=>{
+              var dbOk = rows;
+              if(!dbOk){
+                var dbError = {
+                    "name": 'DB',
+                    "errors": {}
+                };
+                dbError.errors.db = { message: 'Cannot insert information in transaction table' };
+                return res.json(result.successFalse(dbError));
+              }
+            });
+          });
+        }
+        var data = {message : "Success!"};
+        return res.json(result.successTrue(data));
+      });
     }
   });
 });
